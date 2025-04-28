9db6f539f152240ee7671a8412aa1e1fdc379453
zs <zs@lukai.com>
2025-04-28 17:00:09 +0900

    123

---
+++ src/com/lukai/git/Human.java
@@ -0,0 +1,16 @@
+package com.lukai.git;
+
+public class Human {
+
+    private String name;
+    private int age;
+
+
+    public Human() {
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
